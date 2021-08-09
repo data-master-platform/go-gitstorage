67459e4e345a8430848c8d47dd39848def4cbe5a
bjorn <bjmooijekind@gmail.com>
2021-08-09 10:47:03 +0200

    adding wait

--- wait-for-gogs.sh
+++ wait-for-gogs.sh
@@ -6,7 +6,7 @@ cmd="$@"
 while true ; do
     VAR0=$(curl -s -i http://gogs:3000/api/v1/users/search | awk 'NR==1{print $2}')
 
-    echo $(curl -s -i http://gogs:3000/api/v1/users/search)
+    echo $(curl -i http://gogs:3000/api/v1/users/search)
 
     echo ${VAR0}
     echo 200
