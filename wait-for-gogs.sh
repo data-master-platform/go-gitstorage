0b683eabcf42567144cb3be8c70649725464515b
bjorn <bjmooijekind@gmail.com>
2021-08-09 13:33:19 +0200

    adding some random

--- wait-for-gogs.sh
+++ wait-for-gogs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true ; do
-    VAR0=$(curl -s -i http://localhost:3000/api/v1/users/search | awk 'NR==1{print $2}')
+    VAR0=$(curl -s -i http://gogs:3000/api/v1/users/search | awk 'NR==1{print $2}')
     if [ "$VAR0" -eq 200 ]; then
         break
     fi
