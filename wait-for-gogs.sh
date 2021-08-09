be98883893b3acda384b0f683d60581590b49263
bjorn <bjmooijekind@gmail.com>
2021-08-09 10:00:12 +0200

    adding wait

---
+++ wait-for-gogs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cmd="$@"
+
+while true ; do
+    VAR0=$(curl -s -i http://gogs:3000/api/v1/users/search | awk 'NR==1{print $2}')
+
+    echo ${VAR0}
+    echo 200
+    if [ "$VAR0" -eq 200 ]; then
+        break
+    fi
+    echo "sleeping"
+    sleep 2
+done
+
+echo "yes"
+exec $cmd
